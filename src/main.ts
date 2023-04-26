a258f448508d181552e97e26a5da29825f160a15
joams mogro <joamsmg@gmail.com>
2023-04-26 19:14:20 -0400

    unfinal version10

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = +process.env.PORT || 3000;
-
+  const port = parseInt(process.env.PORT) || 3000;
   app.setGlobalPrefix('api');
   app.useGlobalPipes(
     new ValidationPipe({
